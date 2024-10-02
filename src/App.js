8bd379efa86a1fe2577ac219242681ac5dd6f2e1
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2024-10-02 21:43:01 +0530

    refactor: extract calculator component to separate file

--- src/App.js
+++ src/App.js
@@ -1,46 +1,5 @@
-import React, { useState } from 'react';
-
-function Calculator() {
-  const [display, setDisplay] = useState('0'); 
-
-  const handleButtonClick = (value) => {
-    if (value === "=") {
-      try {
-        setDisplay(eval(display)); 
-      } catch {
-        setDisplay("Error"); 
-      }
-    } else if (value === "C") {
-      setDisplay('0'); 
-    } else {
-      setDisplay(display === '0' ? value : display + value); 
-    }
-  };
-
-  
-  const buttons = [
-    "1", "2", "3", 
-    "4", "5", "6", 
-    "7", "8", "9", 
-    "0", "+", "-", 
-    "*", "/", "=", 
-    "C"
-  ];
-
-  return (
-    <div className="calculator">
-      <h2>Calculator</h2>
-      <div className="display">{display || 0}</div>
-      <div className="buttons">
-        {buttons.map((button) => (
-          <button key={button} onClick={() => handleButtonClick(button)}>
-            {button}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
-}
+import React from "react";
+import Calculator from "./Calculator";
 
 function App() {
 	return (
@@ -51,5 +10,3 @@ function App() {
 }
 
 export default App;
-
-
